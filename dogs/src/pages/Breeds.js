f9e3d50a705456054bdc37d2bd731662c976fafe
arcadia-ego <arcadia-ego@github.com>
2018-03-15 13:20:29 -0700

    not displaying yet

--- dogs/src/pages/Breeds.js
+++ dogs/src/pages/Breeds.js
@@ -20,6 +20,27 @@ class Breeds extends Component {
 
     return breeds.filter(breed => choiceBreeds[breed]);
   }
+
+  componentDidMount() {
+    axios.get('https://dog.ceo/api/breeds/list/all')
+    .then(response => {
+        console.log(response);
+        this.setState({ breeds: response.data.message });
+        })
+    .catch(error => {
+        console.log(`There was an error getting dogs: ${error}`);
+        });
+      }
+
+render() {
+  console.log(this.state.breeds);
+  return(
+    <div>
+      {/* <Options props={this.state.breeds} /> */}
+    </div>
+  )
+}
+
 }
 
 export default Breeds;
